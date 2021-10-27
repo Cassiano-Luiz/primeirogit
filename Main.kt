abfbc96e2202ed331abcf6fb88fca5bf59ed4362
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-27 16:20:51 -0300

    Criar 0
    5 - Crie um programa que leia um número do teclado até que encontre um número igual a zero. No final, mostre:
    
    - A soma dos números digitados.
    - A média deles
    - O menor número digitado
    - O maior número digitado

--- Main.kt
+++ Main.kt
@@ -1,7 +1,12 @@
 fun main(args: Array<String>) {
-    print("digite um valor para calcularmos sua tabuada:")
-    var num = readLine()!!.toInt()
-    for (i in 1 downTo 10) {
-        println("$num x $i =${num * i}")
-    }
-     }
+    println("Hello World!")
+    var menor = 0
+    var media = 5
+    var maior = 10
+    print("digite um numero para fazermos a soma de 0 até ele.")
+    menor = readLine()!!.toInt()
+
+        media++
+        maior += media
+    while(media <= menor)
+        println("a soma de todos os numeros de 0 até $menor é $maior") }
