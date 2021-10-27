dfbaba4fa3e54bbb8ceac5f15236e2bdcdec1dd0
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-27 16:18:28 -0300

    Tabuada invertida
    1 - Faça um programa que leia um valor e imprima sua tabuada (aceite apenas números entre 1 e 10). Imprima, logo em seguida, a tabuada ao contrário

--- Main.kt
+++ Main.kt
@@ -1,12 +1,7 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    var menor = 0
-    var media = 5
-    var maior = 10
-    print("digite um numero para fazermos a soma de 0 até ele.")
-    menor = readLine()!!.toInt()
-
-        media++
-        maior += media
-    while(media <= menor)
-        println("a soma de todos os numeros de 0 até $menor é $maior") }
+    print("digite um valor para calcularmos sua tabuada:")
+    var num = readLine()!!.toInt()
+    for (i in 1 downTo 10) {
+        println("$num x $i =${num * i}")
+    }
+     }
