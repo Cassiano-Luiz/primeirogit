597f2253a36b992274bed74e6accb929c7bb38f7
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-27 08:32:01 -0300

    estruturawhen
    exercicio 4

--- Main.kt
+++ Main.kt
@@ -1,20 +1,21 @@
-import kotlin.math.pow
+        fun main(args: Array<String>) {
+        var num = readLine()!!.toInt() {
+        println("digite um numero para descobrirmos o mês do ano.")
+        print("digite um valor entre 1 e 12 ")
+        var num = readLine()!!.toInt()
+        when (num) {
 
-fun main(args: Array<String>) {
-
-    var diam = 0
-    var area = 0.0
-    var perim = 0.0
-    var pi = 3.14
-
-    print("digite o diâmetro do circulo:")
-    diam = readLine()!!.toInt()
-
-    var raio: Double = diam.toDouble()/2
-
-    area = pi * raio.pow(2)
-    perim = (raio * 2) * pi
-
-    println("\nCom base no diêmentro $diam, temos que a área do circulo "+ "é igual a $area e seu perimetro é igual a $perim")
-
-}
+        1 -> println("janeiro")
+        2 -> println("feveiro")
+        3 -> println("março")
+        4 -> println("abril")
+        5 -> println("maio")
+        6 -> println("junho")
+        7 -> println("julho")
+        8 -> println("agosto")
+        9 -> println("setembro")
+        10 -> println("outubro")
+        11 -> println("novembro")
+        12 -> println("dezembro")
+        else -> println("valor invalido")
+        }
