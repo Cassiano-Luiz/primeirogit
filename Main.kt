bfe640d775d26bda86121cd7664d52587ddcb761
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-27 16:15:28 -0300

    Criar 0

--- Main.kt
+++ Main.kt
@@ -1,21 +1,12 @@
-        fun main(args: Array<String>) {
-        var num = readLine()!!.toInt() {
-        println("digite um numero para descobrirmos o mês do ano.")
-        print("digite um valor entre 1 e 12 ")
-        var num = readLine()!!.toInt()
-        when (num) {
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var menor = 0
+    var media = 5
+    var maior = 10
+    print("digite um numero para fazermos a soma de 0 até ele.")
+    menor = readLine()!!.toInt()
 
-        1 -> println("janeiro")
-        2 -> println("feveiro")
-        3 -> println("março")
-        4 -> println("abril")
-        5 -> println("maio")
-        6 -> println("junho")
-        7 -> println("julho")
-        8 -> println("agosto")
-        9 -> println("setembro")
-        10 -> println("outubro")
-        11 -> println("novembro")
-        12 -> println("dezembro")
-        else -> println("valor invalido")
-        }
+        media++
+        maior += media
+    while(media <= menor)
+        println("a soma de todos os numeros de 0 até $menor é $maior") }
