a63ea69d3b4f6c1498f39322ae43fd8614920ddc
Cassiano-Luiz <92732922+Cassiano-Luiz@users.noreply.github.com>
2021-10-28 16:53:43 -0300

    Calculadora

--- Main.kt
+++ Main.kt
@@ -1,12 +1,29 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    var menor = 0
-    var media = 5
-    var maior = 10
-    print("digite um numero para fazermos a soma de 0 até ele.")
-    menor = readLine()!!.toInt()
 
-        media++
-        maior += media
-    while(media <= menor)
-        println("a soma de todos os numeros de 0 até $menor é $maior") }
+    adicao(1, 2)
+    subtracao(3,4)
+    multiplicacao(5,6)
+    divisao(7,8)
+    potencia
+}
+
+   fun adicao( num1: int, num2: int){
+       var result = num1 + num2
+       print("o resultado da adição é: $result ")
+   }
+    fun subtracao ( num1: int, num2: int){
+        var result =num1 - num2
+        print("o resultado da subtração é: $result")
+    }
+    fun multiplicacao (num1: int, num2: int) {
+        var result =num1 * num2
+        println("o resultado da multiplicação é:$result")
+        }
+    fun divisao(num1: double, num2: double){
+        var result = num1 / num 2
+        println("o resultado da divisão é: $result")
+    }
+    fun potencia(num1: double, num2: double){
+        var result = num1 / num2
+        println("o resultado da potencia é: $result")
+    }
